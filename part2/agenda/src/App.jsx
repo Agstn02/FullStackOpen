831c5765830480a59cb1bfc1be5d5c4961100925
Agustín Córdoba <agochoc1@gmail.com>
2025-02-21 16:26:33 -0300

    2.9 completed

--- part2/agenda/src/App.jsx
+++ part2/agenda/src/App.jsx
@@ -1,18 +1,15 @@
 import { useState } from 'react'
 
 const App = () => {
-  const App = () => {
     const [persons, setPersons] = useState([
       { name: 'Arto Hellas', number: '040-123456', id: 1 },
       { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
       { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
       { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
     ])
-  
-    // ...
-  }
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [filter, setFilter] = useState('')
 
   //Funciones
 
@@ -32,6 +29,7 @@ const App = () => {
   }
   const handleNewName = e => setNewName(e.target.value)
   const handleNewNumber = e => setNewNumber(e.target.value)
+  const handleFilter = e => setFilter(e.target.value)
 
   return (
     <div>
@@ -47,8 +45,12 @@ const App = () => {
           <button type="submit">add</button>
         </div>
       </form>
+      <div>
+        filter shown with <input type='text' onChange={handleFilter} value={filter} />
+      </div>
+
       <h2>Numbers</h2>
-      {persons.map( person => <p key={person.id}>{person.name} - {person.number}</p>)}
+      {persons.filter( w => w.name.toLocaleLowerCase().includes(filter.toLocaleLowerCase())).map( person => <p key={person.id}>{person.name} - {person.number}</p>)}
     </div>
   )
 }
